6e871c4945c7c5173d0d7af543a705321e441a3e
bbest <ben@ecoquants.com>
2018-01-26 11:39:07 -0800

    + install-R-packages.R

---
+++ install-R-packages.R
@@ -0,0 +1,33 @@
+# packages on CRAN
+packages_cran <- c(
+  "devtools", 
+  "tidyverse", # includes: ggplot2, dplyr, tidyr, readr, purrr, tibble, stringr, forcats
+  "lubridate", "rlang", "magrittr", "glue", "readxl", "rvest",
+  "rgdal", "raster", "sp", "sf", 
+  "knitr", "rmarkdown", 
+  "htmlwidgets", "DT", "dygraphs", "plotly", "leaflet", "crosstalk", "mapview", "mapedit",
+  "uuid", "finch")
+
+# packages on Github
+packages_github = c(
+  "iobis/obistools", "iobis/robis",
+  "ropensci/taxize", "ropensci/taxizesoap")
+
+# install CRAN packages if needed
+for (p in packages_cran){
+  packages_installed <- rownames(installed.packages())
+  if (!p %in% packages_installed){
+    message("CRAN package ", p, " not found locally. Installing...")
+    install.packages(p)
+  }
+}
+
+# install packages from Github if needed
+for (owner_repo in packages_github){
+  packages_installed <- rownames(installed.packages())
+  p <- stringr::str_split(owner_repo, "/", simplify=T)[2]
+  if (!p %in% packages_installed){
+    message("Github package ", owner_repo, " not found locally. Installing...")
+    devtools::install_github(owner_repo)
+  }
+}
